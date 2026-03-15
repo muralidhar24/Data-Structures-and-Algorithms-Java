1fe40fb521fab22d3b481e7fad780cc072fb1141
Dasari Muralidhar <153696275+muralidhar24@users.noreply.github.com>
2026-03-15 19:57:59 +0530

    first commit

---
+++ Arrays/twosum.java
@@ -0,0 +1,26 @@
+
+// Problem: Two Sum
+// Platform: LeetCode
+// Difficulty: Easy
+// Approach: Brute Force (Nested Loop)
+// Time Complexity: O(n²)
+// Space Complexity: O(1)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i=0; i<nums.length; i++)
+        {
+            for(int j = i+1; j<nums.length; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
