96411dde442fefbfd1f37aeafc60086e247d3ec7
Dasari Muralidhar <153696275+muralidhar24@users.noreply.github.com>
2026-03-21 20:58:54 +0530

    first commit

---
+++ Strings/Reverse String.java
@@ -0,0 +1,24 @@
+
+// Problem: Reverse String
+// Platform: LeetCode
+// Difficulty: Easy
+// Approach: Two Pointer (Swap characters from both ends)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length-1;
+
+        while(left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
