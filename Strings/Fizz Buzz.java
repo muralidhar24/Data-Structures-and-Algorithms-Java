9e451349df79c321f554de1793711c987036cc57
Dasari Muralidhar <153696275+muralidhar24@users.noreply.github.com>
2026-03-22 12:01:35 +0530

    first commit

---
+++ Strings/Fizz Buzz.java
@@ -0,0 +1,30 @@
+
+// Problem: Fizz Buzz
+// Platform: LeetCode
+// Difficulty: Easy
+// Approach: Iteration with conditional checks (modulo)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+
+        for(int i=1; i<=n; i++) {
+
+            if(i%3 == 0 && i%5 == 0)
+                list.add("FizzBuzz");
+            
+            else if(i%3 == 0) 
+                list.add("Fizz");
+
+            else if(i%5 == 0)
+                list.add("Buzz");
+            
+            else
+                list.add(""+i);
+
+        }
+        return list;
+    }
+}
