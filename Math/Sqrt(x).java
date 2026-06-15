933e5f136f88aa3dca5c9cf0a5b3d4ffb4b2f8cf
Dasari Muralidhar <153696275+muralidhar24@users.noreply.github.com>
2026-06-15 18:48:45 +0530

    first commit

---
+++ Math/Sqrt(x).java
@@ -0,0 +1,16 @@
+
+// Problem: Sqrt(x)
+// Platform: LeetCode
+// Difficulty: Easy
+// Approach: Use Java's built-in Math.sqrt()
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+// Optimal: ⚠️ Accepted, but not the intended interview solution
+
+class Solution {
+    public int mySqrt(int x) {
+
+        return (int) Math.sqrt(x);
+
+    }
+}
