a8b89ab36ac8c5da45346fee7b24bad358a7afa7
Dasari Muralidhar <153696275+muralidhar24@users.noreply.github.com>
2026-09-06 18:31:36 +0530

    first commit

---
+++ Arrays/Search in Rotated Sorted Array Optimized.java
@@ -0,0 +1,43 @@
+
+// Problem: Search in Rotated Sorted Array
+// Platform: LeetCode
+// Difficulty: Medium
+// Approach: Modified Binary Search
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+// Optimal: Yes ✅
+
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else {
+
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
